40a6653a85623566f3f9eef26d46885ffaffa1e9
ilan-gold <ilanbassgold@gmail.com>
2020-01-30 15:10:06 -0500

    Update deployment

---
+++ webpack.component.prod.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index_bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-react']
+                }
+            }
+        },
+        {
+            test: /\.(png|jpg|gif)$/,
+            use: [
+                {
+                    loader: 'file-loader'
+                }
+            ]
+        }
+        ]
+    }
+}
