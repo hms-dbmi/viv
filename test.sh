0d61e9e246c59e6bd8734d8b730f1a39c41b777d
mccalluc <mccalluc@users.noreply.github.com>
2020-02-04 13:01:50 -0500

    Add prettier to test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,9 @@ start() { echo travis_fold':'start:$1; echo $1; }
 end() { echo travis_fold':'end:$1; }
 die() { set +v; echo "$*" 1>&2 ; sleep 1; exit 1; }
 
-
+start prettier
+node_modules/prettier/bin-prettier.js --check **/*.js
+end prettier
 
 start changelog
 if [ "$TRAVIS_BRANCH" != 'master' ]; then
