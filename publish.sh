d38d692268f9209a57f2077f03c066e4fb476626
ilan-gold <ilanbassgold@gmail.com>
2020-02-04 16:24:05 -0500

    Reverse order of publishing so failure of the first halts the second

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
-npm run-script build-site
-aws s3 cp --recursive dist s3://image-viewer.vitessce.io
-echo "New demo site at image-viewer.vitessce.io"
-
 npm run-script build-component
 npm publish --access public
 echo "Published npm demo"
+
+npm run-script build-site
+aws s3 cp --recursive dist s3://image-viewer.vitessce.io
+echo "New demo site at image-viewer.vitessce.io"
