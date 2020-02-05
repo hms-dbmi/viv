4ebf83d6ad75b18e44dda82da2d977ab853de5cf
mccalluc <mccalluc@users.noreply.github.com>
2020-02-04 20:05:11 -0500

    s/Base/base/: Revenger of the Mac FS quirk

--- src/layers/microscopy-viewer-layer/microscopy-viewer-layer.js
+++ src/layers/microscopy-viewer-layer/microscopy-viewer-layer.js
@@ -1,38 +1,42 @@
-import {CompositeLayer} from '@deck.gl/core';
-import {fromUrl, Pool, getDecoder } from 'geotiff/dist/geotiff.bundle.min.js';
-import {MicroscopyViewerLayerBase} from './microscopy-viewer-layer-Base'
-import {getTiffConnections,getZarrConnections} from './data-utils'
+import { CompositeLayer } from '@deck.gl/core';
+import { fromUrl, Pool, getDecoder } from 'geotiff/dist/geotiff.bundle.min.js';
+import { MicroscopyViewerLayerBase } from './microscopy-viewer-layer-base';
+import { getTiffConnections, getZarrConnections } from './data-utils';
 
 export class MicroscopyViewerLayer extends CompositeLayer {
-
-  initializeState(){
+  initializeState() {
     this.state = {
       connections: null,
       pool: null
-    }
+    };
   }
 
-  shouldUpdateState({changeFlags}) {
-     return changeFlags.somethingChanged;
-   }
+  shouldUpdateState({ changeFlags }) {
+    return changeFlags.somethingChanged;
+  }
 
-  updateState(){
+  updateState() {
     this.props.useTiff
-    ? !this.state.connections && getTiffConnections({...this.props}).then((connections) => {
-      this.setState({connections, pool: new Pool()})
-    })
-    : !this.state.connections && getZarrConnections({...this.props}).then((connections) => {
-      this.setState({connections})
-    })
-
+      ? !this.state.connections &&
+        getTiffConnections({ ...this.props }).then(connections => {
+          this.setState({ connections, pool: new Pool() });
+        })
+      : !this.state.connections &&
+        getZarrConnections({ ...this.props }).then(connections => {
+          this.setState({ connections });
+        });
   }
 
   renderLayers() {
-    const layers = this.state.connections ? new MicroscopyViewerLayerBase({connections: Object.assign({},...this.state.connections), pool: this.state.pool, ...this.props}) : []
-    return layers
+    const layers = this.state.connections
+      ? new MicroscopyViewerLayerBase({
+          connections: Object.assign({}, ...this.state.connections),
+          pool: this.state.pool,
+          ...this.props
+        })
+      : [];
+    return layers;
   }
-
 }
-
 
 MicroscopyViewerLayer.layerName = 'MicroscopyViewerLayer';
