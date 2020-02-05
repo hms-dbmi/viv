62319e4c1897cae14c40826c63849997620910ae
Chuck McCallum <mccalluc@users.noreply.github.com>
2020-02-05 13:26:40 -0500

    Make everything prettier (again) (#55)
    * Make everything prettier
    
    * CHANGELOG

--- webpack.component.prod.js
+++ webpack.component.prod.js
@@ -1,35 +1,36 @@
-const path = require("path");
+const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   devtool: 'source-map',
-    entry: "./src/index.js",
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: 'index.js',
-        library: 'vitessce-image-viewer',
-        libraryTarget: 'umd'
-    },
-    externals: [nodeExternals()],
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-react']
-                }
-            }
-        },
-        {
-            test: /\.(png|jpg|gif)$/,
-            use: [
-                {
-                    loader: 'file-loader'
-                }
-            ]
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.js',
+    library: 'vitessce-image-viewer',
+    libraryTarget: 'umd'
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
         }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader'
+          }
         ]
-    }
-}
+      }
+    ]
+  }
+};
