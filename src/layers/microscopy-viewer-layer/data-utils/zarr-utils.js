8d3c583c13da012bd1ffcd0e86b841df9334b546
mccalluc <mccalluc@users.noreply.github.com>
2020-02-04 19:49:08 -0500

    Use arrow syntax Also, changes from prettier which seem good, but not sure why they did not happen earlier.

--- src/layers/microscopy-viewer-layer/data-utils/zarr-utils.js
+++ src/layers/microscopy-viewer-layer/data-utils/zarr-utils.js
@@ -1,48 +1,57 @@
 import { slice, openArray } from 'zarr';
 
 async function getData({ arr, channel, tileSize, x, y, stride, tilingWidth }) {
-  const arrSlice = slice(stride * tilingWidth * y + stride * x, stride * tilingWidth * y + stride * (x + 1));
+  const arrSlice = slice(
+    stride * tilingWidth * y + stride * x,
+    stride * tilingWidth * y + stride * (x + 1)
+  );
   const dataSlice = await arr.get([arrSlice]);
   const { data } = dataSlice;
-  const dataObj = {}
-  dataObj[channel] = data
+  const dataObj = {};
+  dataObj[channel] = data;
   return dataObj;
 }
 
 export function loadZarr({ connections, tileSize, x, y, z, imageWidth }) {
-  const tilingWidth = Math.ceil(imageWidth / (tileSize * (2 ** z)));
+  const tilingWidth = Math.ceil(imageWidth / (tileSize * 2 ** z));
   const stride = tileSize * tileSize;
-  const channelPromises = Object.keys(connections).map((channel) => {
+  const channelPromises = Object.keys(connections).map(channel => {
     return getData({
-      arr: connections[channel][z], channel, tileSize, x, y, stride, tilingWidth,
+      arr: connections[channel][z],
+      channel,
+      tileSize,
+      x,
+      y,
+      stride,
+      tilingWidth
     });
   });
-  return Promise.all(channelPromises).then((dataList) => {
-    const orderedList = []
-    const dataObj = Object.assign({}, ...dataList)
-    Object.keys(dataObj).sort().forEach(function(key) {
-      orderedList.push(dataObj[key]);
-    })
+  return Promise.all(channelPromises).then(dataList => {
+    const orderedList = [];
+    const dataObj = Object.assign({}, ...dataList);
+    Object.keys(dataObj)
+      .sort()
+      .forEach(key => orderedList.push(dataObj[key]));
     return orderedList;
   });
 }
 
-export function getZarrConnections({sourceChannels, maxZoom}) {
-  const zarrConnections = Object.keys(sourceChannels).map(async (channel) => {
-    const zarrObj = {}
-    for(let i = 0; i < -maxZoom; i++) {
+export function getZarrConnections({ sourceChannels, maxZoom }) {
+  const zarrConnections = Object.keys(sourceChannels).map(async channel => {
+    const zarrObj = {};
+    for (let i = 0; i < -maxZoom; i++) {
       const zarr = await openArray({
         store: `${sourceChannels[channel]}/`,
         path: `pyramid_${i}.zarr`,
-        mode: 'r',
-      })
-      if(!zarrObj[channel]) {
-        zarrObj[channel] = [zarr]
+        mode: 'r'
+      });
+      if (!zarrObj[channel]) {
+        zarrObj[channel] = [zarr];
       } else {
-        zarrObj[channel].push(zarr)
+        zarrObj[channel].push(zarr);
       }
     }
-    return zarrObj
-  })
-  return Promise.all(zarrConnections)
+    return zarrObj;
+  });
+  return Promise.all(zarrConnections);
 }
--- src/layers/microscopy-viewer-layer/microscopy-viewer-layer-base.js
+++ src/layers/microscopy-viewer-layer/microscopy-viewer-layer-base.js
@@ -1,22 +1,22 @@
-import {BaseTileLayer} from '@deck.gl/layers';
-import {Texture2D} from '@luma.gl/webgl'
+import { BaseTileLayer } from '@deck.gl/layers';
+import { Texture2D } from '@luma.gl/webgl';
 import GL from '@luma.gl/constants';
 import { COORDINATE_SYSTEM } from 'deck.gl';
 import { XRLayer } from '../xr-layer';
-import {tileToBoundingBox,getTileIndices} from './tiling-utils';
+import { tileToBoundingBox, getTileIndices } from './tiling-utils';
 
 import { loadZarr, loadTiff } from './data-utils';
 
-const defaultProps = { ...BaseTileLayer.defaultProps, id: `microscopy-tile-layer`,
+const defaultProps = {
+  ...BaseTileLayer.defaultProps,
+  id: `microscopy-tile-layer`,
   pickable: false,
   coordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
   maxZoom: 0,
   onViewportLoad: false,
-  renderSubLayers: (props) => {
+  renderSubLayers: props => {
     const {
-      bbox: {
-        west, south, east, north,
-      },
+      bbox: { west, south, east, north }
     } = props.tile;
     const { sliderValues, data, colorValues } = props;
     const xrl = new XRLayer(props, {
@@ -27,68 +27,85 @@ const defaultProps = { ...BaseTileLayer.defaultProps, id: `microscopy-tile-layer
       sliderValues,
       colorValues,
       bounds: [west, south, east, north],
-      visible: true,
+      visible: true
     });
     return xrl;
-  },};
+  }
+};
 
 export class MicroscopyViewerLayerBase extends BaseTileLayer {
-
   constructor(props) {
-    const minZoom = Math.floor(-1 * Math.log2(Math.max(props.imageHeight, props.imageWidth)));
-    const {sliderValues, colorValues} = props
-    const orderedSliderValues = []
-    let orderedColorValues = []
-    Object.keys(sliderValues).sort().forEach(function(key) {
-      orderedSliderValues.push(sliderValues[key]);
-    })
-    Object.keys(colorValues).sort().forEach(function(key) {
-      orderedColorValues.push(colorValues[key]);
-    })
-    var diff = 6 - orderedSliderValues.length
+    const minZoom = Math.floor(
+      -1 * Math.log2(Math.max(props.imageHeight, props.imageWidth))
+    );
+    const { sliderValues, colorValues } = props;
+    const orderedSliderValues = [];
+    let orderedColorValues = [];
+    Object.keys(sliderValues)
+      .sort()
+      .forEach(key => orderedSliderValues.push(sliderValues[key]));
+    Object.keys(colorValues)
+      .sort()
+      .forEach(key => orderedColorValues.push(colorValues[key]));
+    var diff = 6 - orderedSliderValues.length;
     for (var i = 0; i < diff; i++) {
-      orderedSliderValues.push([0,65535]);
+      orderedSliderValues.push([0, 65535]);
     }
-    var diff = 6 - orderedColorValues.length
+    var diff = 6 - orderedColorValues.length;
     for (let j = 0; j < diff; j++) {
-      orderedColorValues.push([0,0,0]);
+      orderedColorValues.push([0, 0, 0]);
     }
-    orderedColorValues = orderedColorValues.map(color => color.map(ch => ch / 255))
+    orderedColorValues = orderedColorValues.map(color =>
+      color.map(ch => ch / 255)
+    );
     // flatten for use on shaders
-    var flatSliderValues = [].concat.apply([], orderedSliderValues)
+    var flatSliderValues = [].concat.apply([], orderedSliderValues);
     const getZarr = ({ x, y, z }) => {
       return loadZarr({
-        x, y, z: -1 * z, ...props,
+        x,
+        y,
+        z: -1 * z,
+        ...props
       });
-    }
+    };
     const getTiff = ({ x, y, z }) => {
       return loadTiff({
-        x, y, z: -1 * z, ...props,
+        x,
+        y,
+        z: -1 * z,
+        ...props
       });
-    }
-    const getTileData = (props.useZarr && getZarr) ||
-                        (props.useTiff && getTiff) ||
-                        props.getTileData
+    };
+    const getTileData =
+      (props.useZarr && getZarr) ||
+      (props.useTiff && getTiff) ||
+      props.getTileData;
     const overrideValuesProps = {
-       ...props, sliderValues: flatSliderValues,
-        colorValues: orderedColorValues,
-        minZoom,
-        getTileData,
-        getTileIndices: (viewport, maxZoom, minZoom) => {
-          return getTileIndices({
-            viewport, maxZoom, minZoom, ...props,
-          });
-        },
-        tileToBoundingBox: (x, y, z) => {
-          return tileToBoundingBox({
-            x, y, z, ...props,
-          });
-        },
-    }
-    const layerProps = { ...defaultProps, ...overrideValuesProps}
-    super(layerProps)
+      ...props,
+      sliderValues: flatSliderValues,
+      colorValues: orderedColorValues,
+      minZoom,
+      getTileData,
+      getTileIndices: (viewport, maxZoom, minZoom) => {
+        return getTileIndices({
+          viewport,
+          maxZoom,
+          minZoom,
+          ...props
+        });
+      },
+      tileToBoundingBox: (x, y, z) => {
+        return tileToBoundingBox({
+          x,
+          y,
+          z,
+          ...props
+        });
+      }
+    };
+    const layerProps = { ...defaultProps, ...overrideValuesProps };
+    super(layerProps);
   }
-
 }
 
 MicroscopyViewerLayerBase.layerName = 'MicroscopyViewerLayerBase';
