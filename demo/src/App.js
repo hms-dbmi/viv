495506429fb02d41119875e479cb613e057a6774
mccalluc <mccalluc@users.noreply.github.com>
2020-02-04 20:27:11 -0500

    Install scss dependencies

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,93 +1,95 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
 import Slider from '@material-ui/core/Slider';
 import { withStyles } from '@material-ui/core/styles';
-import { MicroscopyViewer } from '../../src'
-import { source } from './source-info'
-import './App.css';
+import { MicroscopyViewer } from '../../src';
+import { source } from './source-info';
+import './App.scss';
 
 const RedSlider = withStyles({
   root: {
-    color: 'red',
+    color: 'red'
   }
-})(Slider)
+})(Slider);
 const BlueSlider = withStyles({
   root: {
-    color: 'blue',
+    color: 'blue'
   }
-})(Slider)
+})(Slider);
 const GreenSlider = withStyles({
   root: {
-    color: 'green',
+    color: 'green'
   }
-})(Slider)
+})(Slider);
 const OrangeSlider = withStyles({
   root: {
-    color: 'orange',
+    color: 'orange'
   }
-})(Slider)
+})(Slider);
 
 export default class App extends PureComponent {
-
-  constructor(props){
-    super(props)
-    this.handleRedSliderChange = this.handleRedSliderChange.bind(this)
-    this.handleGreenSliderChange = this.handleGreenSliderChange.bind(this)
-    this.handleBlueSliderChange = this.handleBlueSliderChange.bind(this)
-    this.handleOrangeSliderChange = this.handleOrangeSliderChange.bind(this)
-    this.resize = this.resize.bind(this)
+  constructor(props) {
+    super(props);
+    this.handleRedSliderChange = this.handleRedSliderChange.bind(this);
+    this.handleGreenSliderChange = this.handleGreenSliderChange.bind(this);
+    this.handleBlueSliderChange = this.handleBlueSliderChange.bind(this);
+    this.handleOrangeSliderChange = this.handleOrangeSliderChange.bind(this);
+    this.resize = this.resize.bind(this);
     this.state = {
-      sliderValues:{
+      sliderValues: {
         channel_0: [0, 20000],
         channel_1: [0, 20000],
         channel_2: [0, 20000],
         channel_3: [0, 20000]
       },
-      colorValues:{
+      colorValues: {
         channel_0: [255, 0, 0],
         channel_1: [0, 255, 0],
         channel_2: [255, 128, 0],
-        channel_3: [0, 0, 255],
+        channel_3: [0, 0, 255]
       },
-      viewHeight: window.innerHeight * .9,
-      viewWidth: window.innerWidth * .7
+      viewHeight: window.innerHeight * 0.9,
+      viewWidth: window.innerWidth * 0.7
     };
-    this.max =  65535
-    window.addEventListener("resize", this.resize);
-
+    this.max = 65535;
+    window.addEventListener('resize', this.resize);
   }
 
-  handleRedSliderChange(event, value){
-    this.setState({sliderValues: { ...this.state.sliderValues, channel_0: value}})
-  }
-
-  handleGreenSliderChange(event, value){
-    this.setState({sliderValues: { ...this.state.sliderValues, channel_1: value}})
-  }
-
-  handleBlueSliderChange(event, value){
-    this.setState({sliderValues: { ...this.state.sliderValues, channel_3: value}})
-  }
-
-  handleOrangeSliderChange(event, value){
-    this.setState({sliderValues: { ...this.state.sliderValues, channel_2: value}})
-  }
-
-  resize(){
+  handleRedSliderChange(event, value) {
     this.setState({
-      viewHeight: window.innerHeight * .9,
-      viewWidth: window.innerWidth * .7
+      sliderValues: { ...this.state.sliderValues, channel_0: value }
     });
-  };
+  }
+
+  handleGreenSliderChange(event, value) {
+    this.setState({
+      sliderValues: { ...this.state.sliderValues, channel_1: value }
+    });
+  }
+
+  handleBlueSliderChange(event, value) {
+    this.setState({
+      sliderValues: { ...this.state.sliderValues, channel_3: value }
+    });
+  }
+
+  handleOrangeSliderChange(event, value) {
+    this.setState({
+      sliderValues: { ...this.state.sliderValues, channel_2: value }
+    });
+  }
+
+  resize() {
+    this.setState({
+      viewHeight: window.innerHeight * 0.9,
+      viewWidth: window.innerWidth * 0.7
+    });
+  }
 
   render() {
     const initialViewState = {
       zoom: -5.5,
-      target: [
-        30000,
-        10000,
-        0
-      ],
-    }
+      target: [30000, 10000, 0]
+    };
     const propSettings = {
       useTiff: true,
       imageHeight: source.height * source.tileSize,
@@ -95,15 +97,21 @@ export default class App extends PureComponent {
       tileSize: source.tileSize,
       sourceChannels: source.channels,
       minZoom: Math.floor(
-        -1 * Math.log2(Math.max(source.height * source.tileSize, source.width * source.tileSize)),
+        -1 *
+          Math.log2(
+            Math.max(
+              source.height * source.tileSize,
+              source.width * source.tileSize
+            )
+          )
       ),
       maxZoom: -9
-    }
+    };
     const props = {
       initialViewState,
       ...propSettings,
       ...this.state
-    }
+    };
     return (
       <div>
         <MicroscopyViewer {...props} />
