9bded8315c1114b829952510ee7cae43f0047734
mccalluc <mccalluc@users.noreply.github.com>
2020-02-19 11:23:21 -0500

    Rename to VivViewer so future search and replace is easier

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { MicroscopyViewerLayer } from './layers';
 import { MicroscopyViewer } from './microscopy-viewer';
 
-export { MicroscopyViewerLayer as ViewerLayer, MicroscopyViewer as Viewer };
+export {
+  MicroscopyViewerLayer as VivViewerLayer,
+  MicroscopyViewer as VivViewer
+};
