68dda52f536944f1187e421e9b43ef0c9630a670
ilan-gold <ilanbassgold@gmail.com>
2020-02-28 10:59:50 -0500

    Update build to use rollup

---
+++ rollup.config.js
@@ -0,0 +1,51 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import babel from 'rollup-plugin-babel';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+
+const pkg = require('./package.json');
+
+const libraryName = '@hubmap/vitessce-image-viewer';
+
+function getExternals(pkg) {
+  const {peerDependencies = {}} = pkg;
+  console.log(Object.keys(peerDependencies))
+  return Object.keys(peerDependencies)
+}
+
+export default {
+  input: `src/index.js`,
+  output: [
+    { file: pkg.main, name: libraryName, format: 'umd', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true },
+  ],
+  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+  external: getExternals(pkg),
+  watch: {
+    include: 'src/**',
+  },
+  plugins: [
+    // Allow json resolution
+    json(),
+    babel({
+        exclude: 'node_modules/**',
+        presets: ['@babel/env', '@babel/preset-react']
+    }),
+    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+    commonjs({
+      namedExports: {
+        // left-hand side can be an absolute path, a path
+        // relative to the current directory, or the name
+        // of a module in node_modules
+        'node_modules/geotiff/dist/geotiff.bundle.min.js': [ 'fromUrl', 'Pool' ]
+      }
+    }),
+    // Allow node_modules resolution, so you can use 'external' to control
+    // which external modules to include in the bundle
+    // https://github.com/rollup/rollup-plugin-node-resolve#usage
+    resolve(),
+    // Resolve source maps to the original source
+    sourceMaps(),
+  ],
+};
