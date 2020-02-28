1e578a937b37e805222873cd6830aed0ff1a7e67
ilan-gold <ilanbassgold@gmail.com>
2020-02-28 11:07:08 -0500

    Lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,29 @@
 // thank you https://github.com/gzuidhof/zarr.js/blob/master/rollup.config.ts
 // for giving me a starting point :)
-
+/* eslint-disable import/no-extraneous-dependencies */
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import babel from 'rollup-plugin-babel';
 import sourceMaps from 'rollup-plugin-sourcemaps';
 
-const pkg = require('./package.json');
+const pkgObj = require('./package.json');
 
 const libraryName = '@hubmap/vitessce-image-viewer';
 
 function getExternals(pkg) {
   const { peerDependencies = {} } = pkg;
-  console.log(Object.keys(peerDependencies));
   return Object.keys(peerDependencies);
 }
 
 export default {
   input: `src/index.js`,
   output: [
-    { file: pkg.main, name: libraryName, format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true }
+    { file: pkgObj.main, name: libraryName, format: 'umd', sourcemap: true },
+    { file: pkgObj.module, format: 'es', sourcemap: true }
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: getExternals(pkg),
+  external: getExternals(pkgObj),
   watch: {
     include: 'src/**'
   },
