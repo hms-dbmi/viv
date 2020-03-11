fbffa6ebc26feb44315baa156d3477b4079af446
ilan-gold <43999641+ilan-gold@users.noreply.github.com>
2020-03-11 12:01:25 -0400

    Export static image layer (#113)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import { VivViewerLayer } from './layers';
+import { VivViewerLayer, StaticImageLayer } from './layers';
 import VivViewer from './VivViewer';
 import { createTiffPyramid, createZarrPyramid } from './loaders';
 
-export { VivViewerLayer, VivViewer, createTiffPyramid, createZarrPyramid };
+export {
+  VivViewerLayer,
+  VivViewer,
+  StaticImageLayer,
+  createTiffPyramid,
+  createZarrPyramid
+};
