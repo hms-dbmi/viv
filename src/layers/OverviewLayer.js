353afd49ed6b4560bf6f1320aecc60e69317ba61
ilan-gold <ilanbassgold@gmail.com>
2020-03-31 17:36:39 -0400

    Default Props for new layer

--- src/layers/OverviewLayer.js
+++ src/layers/OverviewLayer.js
@@ -2,6 +2,35 @@ import { CompositeLayer, COORDINATE_SYSTEM } from '@deck.gl/core';
 import { PolygonLayer } from '@deck.gl/layers';
 import StaticImageLayer from './StaticImageLayer';
 
+const defaultProps = {
+  pickable: false,
+  loader: {
+    type: 'object',
+    value: {
+      getRaster: async () => ({ data: [], height: 0, width: 0 }),
+      getRasterSize: () => ({ height: 0, width: 0 }),
+      dtype: '<u2'
+    },
+    compare: true
+  },
+  id: { type: 'string', value: '', compare: true },
+  boundingBox: {
+    type: 'array',
+    value: [
+      [0, 0],
+      [0, 1],
+      [1, 1],
+      [1, 0]
+    ],
+    compare: true
+  },
+  boundingBoxColor: { type: 'array', value: [255, 0, 0], compare: true },
+  boundingBoxOutlineWidth: { type: 'number', value: 10, compare: true },
+  viewportOutlineColor: { type: 'array', value: [0, 255, 0], compare: true },
+  viewportOutlineWidth: { type: 'number', value: 10, compare: true },
+  overviewScale: { type: 'number', value: 1, compare: true }
+};
+
 export default class OverviewLayer extends CompositeLayer {
   renderLayers() {
     const {
@@ -55,4 +84,5 @@ export default class OverviewLayer extends CompositeLayer {
   }
 }
 
-OverviewLayer.layerName = 'VivVOverviewLayeriewerLayer';
+OverviewLayer.layerName = 'OverviewLayer';
+OverviewLayer.defaultProps = defaultProps;
