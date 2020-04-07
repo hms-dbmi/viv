f7916ce0f3c7717114b193ac8b256257c30317ff
Trevor Manz <trevor.j.manz@gmail.com>
2020-04-07 01:51:39 -0400

    update zarr loader

--- src/loaders/zarrLoader.js
+++ src/loaders/zarrLoader.js
@@ -1,97 +1,78 @@
 import { range } from '../layers/VivViewerLayer/utils';
+import { guessRgb, normalizeLoaderSelection } from './utils';
 
 export default class ZarrLoader {
-  constructor(data, isRgb, scale, dimensions) {
+  constructor({
+    data,
+    dimensions,
+    isRgb,
+    scale = 1,
+    translate = { x: 0, y: 0 }
+  }) {
     let base;
     if (Array.isArray(data)) {
-      this.isPyramid = true;
-      this.numLevels = data.length;
       [base] = data;
+      this.numLevels = data.length;
     } else {
-      this.isPyramid = false;
-      this.numLevels = 1;
       base = data;
+      this.numLevels = 1;
     }
-    const { dtype } = base;
-    this.dtype = dtype;
-    this._data = data;
-    this.scale = scale;
-    this.chunkIndex = Array(base.shape.length).fill(0);
-    if (isRgb) {
-      this.channelIndex = base.shape.length - 1;
-      this.xIndex = base.shape.length - 2;
-      this.yIndex = base.shape.length - 3;
-    } else {
-      this.xIndex = base.shape.length - 1;
-      this.yIndex = base.shape.length - 2;
-      this.channelIndex = base.shape.length - 3;
-    }
-    this.channelChunkSize = base.chunks[this.channelIndex];
-    this.dimensions = dimensions;
-    this.dimNames = Object.keys(dimensions);
-    this.tileSize = base.chunks[this.xIndex];
     this.type = 'zarr';
+    this.scale = scale;
+    this.translate = translate;
+    this.isRgb = isRgb || guessRgb(base.shape);
+    this.dimensions = dimensions;
+
+    this._data = data;
+    if (this.isRgb) {
+      this._xIndex = base.shape.length - 2;
+      this._yIndex = base.shape.length - 3;
+    } else {
+      this._xIndex = base.shape.length - 1;
+      this._yIndex = base.shape.length - 2;
+    }
+
+    const { dtype, chunks } = base;
+    this.dtype = dtype;
+    this.tileSize = chunks[this._xIndex];
   }
 
-  get _base() {
+  get isPyramid() {
+    return Array.isArray(this._data);
+  }
+
+  get base() {
     return this.isPyramid ? this._data[0] : this._data;
   }
 
-  setChunkIndex(dimName, index) {
-    const chunkIndex = [...this.chunkIndex];
-    if (typeof dimName === 'string') {
-      // Just change index across one axis
-      const i = this.dimNames.indexOf(dimName);
-      chunkIndex[i] = index;
-    } else if (dimName.length !== index.length) {
-      throw Error(
-        `Dimension names and indicies must be same length when set together`
-      );
-    } else {
-      for (let i = 0; i < dimName.length; i += 1) {
-        const dimIndex = this.dimNames.indexOf(dimName[i]);
-        chunkIndex[dimIndex] = index[i];
-      }
-    }
-    this.chunkIndex = chunkIndex;
+  async getTile({ x, y, z, loaderSelections }) {
+    const source = this._getSource(z);
+    const dataRequests = loaderSelections.map(async key => {
+      const chunkKey = [...key];
+      chunkKey[this._yIndex] = y;
+      chunkKey[this._xIndex] = x;
+      const { data } = await source.getRawChunk(chunkKey);
+      return data;
+    });
+    const data = await Promise.all(dataRequests);
+    return { data, width: this.tileSize, height: this.tileSize };
   }
 
-  async getTile({ x, y, z }) {
-    const source = this.isPyramid ? this._data[z] : this._data;
-
-    const chunkKey = [...this.chunkIndex];
-    chunkKey[this.xIndex] = x;
-    chunkKey[this.yIndex] = y;
-
-    if (this.isRgb) {
-      // Return one interleaved TypedArray
-      const imageTile = await source.getRawChunk(chunkKey);
-      return imageTile.data;
-    }
-
-    const { data } = await source.getRawChunk(chunkKey);
-    // Return Array of TypedArrays
-    if (this.channelChunkSize > 1) {
-      return this._decodeChannels(data);
-    }
-    return [data];
-  }
-
-  async getRaster({ z }) {
-    const source = this.isPyramid ? this._data[z] : this._data;
-    const height = source.shape[this.yIndex];
-    const width = source.shape[this.xIndex];
-    const selection = [...this.chunkIndex];
-    selection[this.xIndex] = null;
-    selection[this.yIndex] = null;
-    if (this.isPyramid) {
-      selection[this.channelIndex] = null;
-    }
-    const { data } = await source.getRaw(selection);
-    if (this.channelChunkSize > 1) {
-      return { data: this._decodeChannels(data), width, height };
-    }
-    return { data: [data], width, height };
+  async getRaster({ z, loaderSelections }) {
+    const source = this._getSource(z);
+    const dataRequests = loaderSelections.map(async key => {
+      const chunkKey = [...key];
+      chunkKey[this._yIndex] = null;
+      chunkKey[this._xIndex] = null;
+      if (this.isRgb) chunkKey[chunkKey.length - 1] = null;
+      const { data } = await source.getRaw(chunkKey);
+      return data;
+    });
+    const data = await Promise.all(dataRequests);
+    const { shape } = source;
+    const width = shape[this._xIndex];
+    const height = shape[this._yIndex];
+    return { data, width, height };
   }
 
   // eslint-disable-next-line class-methods-use-this
@@ -106,19 +87,55 @@ export default class ZarrLoader {
   }
 
   getRasterSize({ z }) {
-    const source = z ? this._data[z] : this._base;
+    const source = this._getSource(z);
     const height = source.shape[this.yIndex];
     const width = source.shape[this.xIndex];
     return { height, width };
   }
 
-  _decodeChannels(chunkData) {
-    // Used for multichannel images where channels are in same chunk.
-    // Separates single TypedArray into multiple channel views.
-    const offset = chunkData.length / this.channelChunkSize;
-    const tileData = range(this.channelChunkSize).map(i =>
-      chunkData.subarray(offset * i, offset * i + offset)
-    );
-    return tileData;
+  getLoaderSelection(loaderSelections) {
+    const selections = Array.isArray(loaderSelections)
+      ? loaderSelections
+      : [loaderSelections];
+
+    const normedSelections = selections.map(this._normalizeSelection);
+    return normedSelections;
+  }
+
+  _getSource(z) {
+    return typeof z === 'number' && this.isPyramid ? this._data[z] : this._data;
+  }
+
+  _normalizeSelection(selectionObj) {
+    const loaderSelection = Array(this.dimensions.length).fill(0);
+    const dimNames = this.dimensions.map(d => d.name);
+    // eslint-disable-next-line no-restricted-syntax
+    for (const [dimName, dimValue] of selectionObj.entries()) {
+      const dimIndex = dimNames.indexOf(dimName);
+
+      if (dimIndex < 0) {
+        throw Error(
+          `Dimension '${dimName}' does not exist on array with dimensions : ${dimNames}`
+        );
+      }
+
+      let valueIndex;
+      const { name, type, values } = this.dimensions[dimIndex];
+      if (
+        typeof dimValue === 'string' &&
+        (type === 'ordinal' || type === 'nominal')
+      ) {
+        valueIndex = values.indexof(dimValue);
+      } else {
+        valueIndex = dimValue;
+      }
+
+      if (valueIndex < 0) {
+        throw Error(`Dimension ${name} does not contain index ${valueIndex}.`);
+      }
+
+      loaderSelection[dimIndex] = valueIndex;
+    }
+    return loaderSelection;
   }
 }
