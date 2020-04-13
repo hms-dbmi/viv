56e127bf2e2e6ca1a7f4a828259cdbed7e0899df
ilan-gold <ilanbassgold@gmail.com>
2020-04-13 18:06:04 -0400

    Revert to class component

--- src/viewers/VivViewer.js
+++ src/viewers/VivViewer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { PureComponent } from 'react';
 import DeckGL from '@deck.gl/react';
 import { getVivId } from '../views/utils';
 
@@ -14,39 +14,27 @@ function isSafari() {
 }
 
 /**
- * This component handles rendering the various views within the DeckGL contenxt.
+ * This class handles rendering the various views within the DeckGL contenxt.
  * @param {Array} layerProps The props for the layers in each view.
+ * @param {Array} randomize Wgether or not to randomize which view goes first (for dynamic rendering).
  * @param {VivView} views The various VivViews to render.
- * @param {Boolean} randomize Whether or not to randomize which view goes first to deck.gl
- * (for dynamic multi-view rendering.)
  * */
-const VivViewer = ({ layerProps, views, randomize }) => {
-  const initialViewStates = {};
-  views.forEach(view => {
-    initialViewStates[view.id] = view.filterViewState({
-      viewState: view.initialViewState
-    });
-  });
-  const [viewStates, setviewStates] = useState(initialViewStates);
-
-  useEffect(() => {
-    setviewStates(prevViewStates => {
-      const newViewStates = {};
-      views.forEach(view => {
-        const { height, width } = view.initialViewState;
-        const currentViewState = prevViewStates[view.id];
-        newViewStates[view.id] = view.filterViewState({
-          viewState: {
-            ...(currentViewState || view.initialViewState),
-            height,
-            width,
-            id: view.id
-          }
-        });
+export default class VivViewer extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewStates: {}
+    };
+    const { viewStates } = this.state;
+    const { views } = this.props;
+    views.forEach(view => {
+      viewStates[view.id] = view.filterViewState({
+        viewState: view.initialViewState
       });
-      return newViewStates;
     });
-  }, [views]);
+    this._onViewStateChange = this._onViewStateChange.bind(this);
+    this.layerFilter = this.layerFilter.bind(this);
+  }
 
   /**
    * This prevents only the `draw` call of a layer from firing,
@@ -57,75 +45,124 @@ const VivViewer = ({ layerProps, views, randomize }) => {
    * @returns {boolean} Whether or not this layer should be drawn in this viewport
    */
   // eslint-disable-next-line class-methods-use-this
-  const layerFilter = ({ layer, viewport }) => {
+  layerFilter({ layer, viewport }) {
     return layer.id.includes(getVivId(viewport.id));
-  };
+  }
 
   /**
    * This updates the viewState as a callback to the viewport changing in DeckGL
    * (hence the need for storing viewState in state).
    */
-  const _onViewStateChange = ({ viewId, viewState, oldViewState }) => {
+  _onViewStateChange({ viewId, viewState, oldViewState }) {
     // Save the view state and trigger rerender
-    setviewStates(prevViewStates => {
-      const newViewStates = {};
+    const { views } = this.props;
+    this.setState(prevState => {
+      const newState = {};
+      const viewStates = {};
       views.forEach(view => {
-        const currentViewState = prevViewStates[view.id];
-        newViewStates[view.id] = view.filterViewState({
+        const currentViewState = prevState.viewStates[view.id];
+        viewStates[view.id] = view.filterViewState({
           viewState: { ...viewState, id: viewId },
           oldViewState,
           currentViewState
         });
       });
-      return newViewStates;
+      newState.viewStates = viewStates;
+      return { ...newState };
     });
-  };
+  }
+
+  /**
+   * This updates the viewStates' height and width with the newest height and
+   * width on any call where the viewStates changes (i.e resize events),
+   * using the previous state (falling back on the view's initial state) for target x and y, zoom level etc.
+   */
+  componentDidUpdate(prevProps) {
+    const { views } = this.props;
+    // Update internal viewState on view changes as well as height and width changes.
+    // Maybe we should add x/y too?
+    if (
+      views.some(
+        (view, i) =>
+          prevProps.views[i] !== view ||
+          view.initialViewState.height !==
+            prevProps.views[i].initialViewState.height ||
+          view.initialViewState.width !==
+            prevProps.views[i].initialViewState.width
+      )
+    ) {
+      // eslint-disable-next-line react/no-did-update-set-state
+      this.setState(prevState => {
+        const newState = {};
+        const viewStates = {};
+        views.forEach(view => {
+          const { height, width } = view.initialViewState;
+          const currentViewState = prevState.viewStates[view.id];
+          viewStates[view.id] = view.filterViewState({
+            viewState: {
+              ...(currentViewState || view.initialViewState),
+              height,
+              width,
+              id: view.id
+            }
+          });
+        });
+        newState.viewStates = viewStates;
+        return { ...newState };
+      });
+    }
+  }
 
   /**
    * This renders the layers in the DeckGL context.
    */
-  const _renderLayers = () => {
+  _renderLayers() {
+    const { viewStates } = this.state;
+    const { views, layerProps } = this.props;
     return views.map((view, i) =>
       view.getLayer({ viewStates, props: layerProps[i] })
     );
-  };
-
-  const deckGLViews = views.map(view => view.getDeckGlView());
-
-  // DeckGL seems to use the first view more than the second for updates
-  // so this forces it to use the others more evenly.  This isn't perfect,
-  // but I am not sure what else to do.  The DeckGL render hooks don't help,
-  // but maybe useEffect() would help?  I couldn't work it out as
-  // The issue is that I'm not sure how React would distinguish between forced updates
-  // from permuting the views array and "real" updates like zoom/pan.
-  // I tried keeping a counter but I couldn't figure out resetting it
-  // without triggering a re-render.
-  if (randomize) {
-    const random = Math.random();
-    const holdFirstElement = deckGLViews[0];
-    // weight has to go to 1.5 because we use Math.round()
-    const randomWieghted = random * 1.49;
-    const randomizedIndex = Math.round(randomWieghted * (views.length - 1));
-    deckGLViews[0] = deckGLViews[randomizedIndex];
-    deckGLViews[randomizedIndex] = holdFirstElement;
   }
-  return !isSafari() ? (
-    <DeckGL
-      glOptions={{ webgl2: true }}
-      layerFilter={layerFilter}
-      layers={_renderLayers()}
-      onViewStateChange={_onViewStateChange}
-      views={deckGLViews}
-      viewState={viewStates}
-    />
-  ) : (
-    <div className="viv-error">
-      <p>
-        Safari does not support WebGL2, which Viv requires. Please use Chrome or
-        Firefox.
-      </p>
-    </div>
-  );
-};
 
-export default VivViewer;
+  render() {
+    /* eslint-disable react/destructuring-assignment */
+    const { views, randomize } = this.props;
+    const { viewStates } = this.state;
+    const deckGLViews = views.map(view => view.getDeckGlView());
+    // DeckGL seems to use the first view more than the second for updates
+    // so this forces it to use the others more evenly.  This isn't perfect,
+    // but I am not sure what else to do.  The DeckGL render hooks don't help,
+    // but maybe useEffect() would help?  I couldn't work it out as
+    // The issue is that I'm not sure how React would distinguish between forced updates
+    // from permuting the views array and "real" updates like zoom/pan.
+    // I tried keeping a counter but I couldn't figure out resetting it
+    // without triggering a re-render.
+    if (randomize) {
+      const random = Math.random();
+      const holdFirstElement = deckGLViews[0];
+      // weight has to go to 1.5 because we use Math.round()
+      const randomWieghted = random * 1.49;
+      const randomizedIndex = Math.round(randomWieghted * (views.length - 1));
+      deckGLViews[0] = deckGLViews[randomizedIndex];
+      deckGLViews[randomizedIndex] = holdFirstElement;
+    }
+    return !isSafari() ? (
+      <DeckGL
+        glOptions={{ webgl2: true }}
+        layerFilter={this.layerFilter}
+        layers={this._renderLayers()}
+        onViewStateChange={this._onViewStateChange}
+        views={deckGLViews}
+        viewState={viewStates}
+      />
+    ) : (
+      <div className="viv-error">
+        <p>
+          Safari does not support WebGL2, which Viv requires. Please use Chrome
+          or Firefox.
+        </p>
+      </div>
+    );
+    /* eslint-disable react/destructuring-assignment */
+  }
+}
