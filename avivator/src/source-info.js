ffc7b7c93feafcb9dd25484d05487abbf89ea284
Trevor Manz <trevor.j.manz@gmail.com>
2020-08-10 19:25:43 -0400

    Update demo links

--- avivator/src/source-info.js
+++ avivator/src/source-info.js
@@ -1,9 +1,9 @@
-const baseUrl = 'https://vitessce-demo-data.storage.googleapis.com/test-data';
+const baseUrl = 'https://viv-demo.storage.googleapis.com';
 
 const sources = [
   {
     // Generated using bioformats2raw and raw2ometiff.
-    path: 'hubmap/pyramid_0.0.2/spraggins.ome.tif',
+    path: 'Vanderbilt-Spraggins-Kidney-MxIF.ome.tif',
     description: 'OME-TIFF Kidney mxIF'
   },
   {
@@ -18,12 +18,12 @@ const sources = [
   },
   {
     // Generated using bioformats2raw and raw2ometiff.
-    path: 'hubmap/test/VAN0008-RK-403-100-PAS_registered.ome.tif',
+    path: 'VAN0008-RK-403-100-PAS_registered.ome.tif',
     description: 'OME-TIFF PAS Donor Image'
   },
   {
     // Generated using bioformats2raw and raw2ometiff
-    path: 'hubmap/pyramid_0.0.2/VAN0003-LK-32-21-AF_preIMS_registered.ome.tif',
+    path: 'VAN0003-LK-32-21-AF_preIMS_registered.ome.tif',
     description: 'OME-TIFF AF Donor Image'
   },
   {
@@ -32,7 +32,7 @@ const sources = [
   },
   {
     // Generated with `bioformats2raw --file_type=zarr --dimension_order='XYZCT'`
-    path: 'Luca-7Color/',
+    path: 'LuCa-7color_Scan1/',
     description: 'Perkin Elmer Luca-7Color_Scan1.qgtiff'
   }
 ];
