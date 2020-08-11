a07f04ea124548c152b86ebd55d7836bf0c80bba
Trevor Manz <trevor.j.manz@gmail.com>
2020-08-11 11:14:42 -0400

    Update demo links (#250)
    * Update demo links
    
    * Type in Zarr source description:
    
    * Remove PAS/AF images
    
    * Consolidate source data

--- avivator/src/source-info.js
+++ avivator/src/source-info.js
@@ -1,9 +1,9 @@
-const baseUrl = 'https://vitessce-demo-data.storage.googleapis.com/test-data';
+const baseUrl = 'https://viv-demo.storage.googleapis.com';
 
 const sources = [
   {
     // Generated using bioformats2raw and raw2ometiff.
-    path: 'hubmap/pyramid_0.0.2/spraggins.ome.tif',
+    path: 'Vanderbilt-Spraggins-Kidney-MxIF.ome.tif',
     description: 'OME-TIFF Kidney mxIF'
   },
   {
@@ -12,28 +12,13 @@ const sources = [
     description: 'CODEX Tile'
   },
   {
-    // Old Faas(?) bioformats pyramid
-    path: 'TONSIL-1_40X.ome.tif',
-    description: 'Legacy Bioformats OME-TIFF pyramid (Fass)'
-  },
-  {
-    // Generated using bioformats2raw and raw2ometiff.
-    path: 'hubmap/test/VAN0008-RK-403-100-PAS_registered.ome.tif',
-    description: 'OME-TIFF PAS Donor Image'
-  },
-  {
-    // Generated using bioformats2raw and raw2ometiff
-    path: 'hubmap/pyramid_0.0.2/VAN0003-LK-32-21-AF_preIMS_registered.ome.tif',
-    description: 'OME-TIFF AF Donor Image'
-  },
-  {
     path: '12448_G1HR_Mesh003.ome.tif',
     description: 'OME-TIFF Covid-19 Primary Gut Epithelial Stem Cells'
   },
   {
     // Generated with `bioformats2raw --file_type=zarr --dimension_order='XYZCT'`
-    path: 'Luca-7Color/',
-    description: 'Perkin Elmer Luca-7Color_Scan1.qgtiff'
+    path: 'LuCa-7color_Scan1/',
+    description: 'Perkin Elmer LuCa-7color_Scan1.qptiff'
   }
 ];
 
