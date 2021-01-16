25a711f0ca2792c2767e69d9d290e26604cfeaed
Trevor Manz <trevor.j.manz@gmail.com>
2021-01-16 16:48:58 -0500

    consolidate missing-types

--- types/missing-types.d.ts
+++ types/missing-types.d.ts
@@ -81,3 +81,28 @@ declare module 'quickselect' {
   function quickselect<T>(arr: ArrayLike<T>, k?: number, left?: number, right?: number, compareFn?: (a: T, b: T) => void) {}
   export default quickselect;
 }
+
+
+/*
+* Adds types for imports from `rollup-plugin-web-worker-loader`
+*
+* import Worker from 'web-worker:./some-url'
+* const worker = new Worker();
+*/
+declare module 'web-worker:*' {
+  class WorkerLoader extends Worker {
+    constructor() {}
+  }
+  export default WorkerLoader;
+}
+
+/*
+* Adds types for files imported by 'rollup-plugin-glslify'
+*
+* import fs from 'my-shader.glsl';
+* (typeof fs === string) === true
+*/
+declare module '**/*.glsl' {
+  const value: string;
+  export default string;
+}
