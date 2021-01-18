1ad538580d74674112ef3c8bfecb52c7c7d1939d
Andreas Girgensohn <andreasg123@users.noreply.github.com>
2021-01-18 10:30:15 -0800

    Update src/views/SideBySideView.js
    Co-authored-by: ilan-gold <ilanbassgold@gmail.com>

--- src/views/SideBySideView.js
+++ src/views/SideBySideView.js
@@ -3,7 +3,7 @@ import { COORDINATE_SYSTEM } from '@deck.gl/core';
 
 import { ScaleBarLayer } from '../layers';
 import VivView from './VivView';
-import { getDetailLayers, getVivId, makeBoundingBox } from './utils';
+import { getImageLayers, getVivId, makeBoundingBox } from './utils';
 /**
  * This class generates a MultiscaleImageLayer and a view for use in the SideBySideViewer.
  * It is linked with its other views as controlled by `linkedIds`, `zoomLock`, and `panLock` parameters.
