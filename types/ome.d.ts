525e7a8aa3fcb1e81371a89748e2464bb7b0acdf
Trevor Manz <trevor.j.manz@gmail.com>
2021-01-17 19:28:13 -0500

    add zarr attrs meta

---
+++ types/ome.d.ts
@@ -0,0 +1,35 @@
+declare module Ome {
+
+  interface Channel {
+    active: boolean;
+    color: string;
+    label: string;
+    window: {
+      min?: number;
+      max?: number;
+      start: number;
+      end: number;
+    };
+  };
+
+  interface Omero {
+    channels: Channel[];
+    rdefs: {
+      defaultT?: number;
+      defaultZ?: number;
+      model: string;
+    };
+    name?: string;
+  };
+
+  interface Multiscale {
+    datasets: { path: string }[];
+    version?: string;
+  };
+
+  interface RootAttrs {
+    omero: Omero;
+    multiscales: Multiscale[];
+  };
+
+}
