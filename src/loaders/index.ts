c242d1479b8c4a3aedae707084079abb6a807252
Trevor Manz <trevor.j.manz@gmail.com>
2021-02-11 14:26:39 -0500

    Export default not * from pixel-sources

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -2,5 +2,5 @@ export { loadOmeTiff } from './tiff';
 export { loadBioformatsZarr, loadOmeZarr } from './zarr';
 export { getChannelStats } from './utils';
 
-export * as TiffPixelSource from './tiff/pixel-source';
-export * as ZarrPixelSource from './zarr/pixel-source';
+export { default as TiffPixelSource } from './tiff/pixel-source';
+export { default as ZarrPixelSource } from './zarr/pixel-source';
