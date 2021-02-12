49fd2178485f6f77af48f45cfc18ac76f7597883
ilan-gold <ilanbassgold@gmail.com>
2021-02-12 13:13:23 -0500

    Update tests.

--- src/views/DetailView.js
+++ src/views/DetailView.js
@@ -17,16 +17,14 @@ export default class DetailView extends VivView {
     const layers = getImageLayers(id, props);
 
     // Inspect the first pixel source for physical sizes
-    const {
-      meta: { physicalSizes }
-    } = loader[0];
-    if (physicalSizes?.x) {
+    if (loader[0]?.meta?.physicalSizes?.x) {
+      const { size, unit } = loader[0].meta.physicalSizes.x;
       layers.push(
         new ScaleBarLayer({
           id: getVivId(id),
           loader,
-          unit: physicalSizes.x.unit,
-          size: physicalSizes.x.size,
+          unit,
+          size,
           viewState: { ...layerViewState, height, width }
         })
       );
