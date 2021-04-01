561e3d2577889c9541b690ec1882fc714fee09c8
ilan-gold <ilanbassgold@gmail.com>
2021-04-01 17:21:15 -0400

    Fix ImageLayer

--- src/layers/ImageLayer.js
+++ src/layers/ImageLayer.js
@@ -41,7 +41,7 @@ const defaultProps = {
  * @property {Array.<Array.<number>>} colorValues List of [r, g, b] values for each channel.
  * @property {Array.<Array.<boolean>>} channelIsOn List of boolean values for each channel for whether or not it is visible.
  * @property {Array} loader PixelSource. Represents an N-dimensional image.
- * @property {Array} loader Selection to be used for fetching data.
+ * @property {Array} loaderSelection Selection to be used for fetching data.
  * @property {number=} opacity Opacity of the layer.
  * @property {string=} colormap String indicating a colormap (default: '').  The full list of options is here: https://github.com/glslify/glsl-colormap#glsl-colormap
  * @property {Array.<Array.<number>>=} domain Override for the possible max/min values (i.e something different than 65535 for uint16/'<u2').
