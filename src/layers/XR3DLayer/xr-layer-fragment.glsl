a448093b3e5d27102a51033d93c56c596c8bda97
ilan-gold <ilanbassgold@gmail.com>
2021-04-14 23:54:06 -0400

    Remove cast.

--- src/layers/XR3DLayer/xr-layer-fragment.glsl
+++ src/layers/XR3DLayer/xr-layer-fragment.glsl
@@ -131,7 +131,6 @@ void main(void) {
 		for (int i = 0; i < _NUM_PLANES; i += 1) {
 			canShow *= max(0., sign(dot(normalClippingPlanes[i], p - offsetClippingPlanes[i])));
 		}
-		canShow = float(canShow);
     float intensityValue0 = canShow * sample_and_apply_sliders(volume0, p, sliderValues[0]);
     float intensityValue1 = canShow * sample_and_apply_sliders(volume1, p, sliderValues[1]);
 		float intensityValue2 = canShow * sample_and_apply_sliders(volume2, p, sliderValues[2]);
