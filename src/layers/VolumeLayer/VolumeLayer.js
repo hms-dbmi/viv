393cb4206f7e6885de3b9af06194c5bb980f25b7
ilan-gold <ilanbassgold@gmail.com>
2021-06-21 10:25:39 -0400

    Clear state (actually)

--- src/layers/VolumeLayer/VolumeLayer.js
+++ src/layers/VolumeLayer/VolumeLayer.js
@@ -69,6 +69,19 @@ const defaultProps = {
  * @ignore
  */
 const VolumeLayer = class extends CompositeLayer {
+  clearState() {
+    this.setState({
+      height: null,
+      width: null,
+      depth: null,
+      data: null,
+      physicalSizeScalingMatrix: null,
+      resolutionMatrix: null,
+      progress: 0,
+      abortController: null
+    });
+  }
+
   finalizeState() {
     this.state.abortController.abort();
   }
@@ -85,7 +98,7 @@ const VolumeLayer = class extends CompositeLayer {
     // Only fetch new data to render if loader has changed
     if (resolutionChanged) {
       // Clear last volume.
-      this.setState({});
+      this.clearState();
     }
     if (loaderChanged || loaderSelectionChanged || resolutionChanged) {
       const {
