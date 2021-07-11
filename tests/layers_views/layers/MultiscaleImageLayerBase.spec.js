2b81e797ca072f252770ac7bc5b6abf0941783ba
Trevor Manz <trevor.j.manz@gmail.com>
2021-07-11 11:33:42 -0400

    bump test deps

--- tests/layers_views/layers/MultiscaleImageLayerBase.spec.js
+++ tests/layers_views/layers/MultiscaleImageLayerBase.spec.js
@@ -1,14 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies, no-unused-expressions */
 import test from 'tape-catch';
-import 'abortcontroller-polyfill/dist/polyfill-patch-fetch';
-import { implementation } from 'promise.prototype.finally';
+import 'abortcontroller-polyfill/dist/abortcontroller-polyfill-only';
 import { generateLayerTests, testLayerAsync } from '@deck.gl/test-utils';
 import { OrthographicView } from '@deck.gl/core';
 import MultiscaleImageLayerBase from '../../../src/layers/MultiscaleImageLayer/MultiscaleImageLayerBase';
-
-// Tape-run uses an older electron that doesn't support this.
-// eslint-disable-next-line no-extend-native
-Promise.prototype.finally = implementation;
 
 test('MultiscaleImageLayerBase', async t => {
   const view = new OrthographicView({
