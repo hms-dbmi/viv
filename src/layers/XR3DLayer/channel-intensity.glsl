db8b0184aeb75e1a06446275466fc452fb337f9c
ilan-gold <ilanbassgold@gmail.com>
2021-08-25 16:01:32 -0400

    Remove last function...

--- src/layers/XR3DLayer/channel-intensity.glsl
+++ src/layers/XR3DLayer/channel-intensity.glsl
@@ -43,12 +43,6 @@
 #pragma glslify: velocity-green = require("glsl-colormap/velocity-green")
 #pragma glslify: cubehelix = require("glsl-colormap/cubehelix")
 
-vec3 hsv_to_rgb(vec3 c) {
-  vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-  vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-  return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-}
-
 float sample_and_apply_sliders(SAMPLER_TYPE channel, vec3 vTexCoord, vec2 sliderValues) {
   float fragIntensity = float(texture(channel, vTexCoord).r);
   float slidersAppliedToIntensity = (fragIntensity - sliderValues[0]) / max(0.0005, (sliderValues[1] - sliderValues[0]));
