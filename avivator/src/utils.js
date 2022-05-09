464245eba27848b5c77bf6462a25942e23ff2b7a
Trevor Manz <trevor.j.manz@gmail.com>
2022-05-09 07:54:45 -1000

    fix(avivator): also disable worker pool for File-based OME-TIFF sources (#597)

--- avivator/src/utils.js
+++ avivator/src/utils.js
@@ -78,7 +78,11 @@ export async function createLoader(
     // OME-TIFF
     if (isOMETIFF(urlOrFile)) {
       if (urlOrFile instanceof File) {
-        const source = await loadOmeTiff(urlOrFile, { images: 'all' });
+        // TODO(2021-05-09): temporarily disable `pool` until inline worker module is fixed.
+        const source = await loadOmeTiff(urlOrFile, {
+          images: 'all',
+          pool: false
+        });
         return source;
       }
       const url = urlOrFile;
