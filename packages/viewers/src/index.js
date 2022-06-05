9a894fdfb7edfc2238e7d237c194984f53bdb672
Trevor Manz <trevor.j.manz@gmail.com>
2022-06-05 18:07:51 -0400

    chore: mostly working docs

--- packages/viewers/src/index.js
+++ packages/viewers/src/index.js
@@ -1,4 +1,5 @@
-export { default as VivViewer } from './VivViewer';
-export { default as PictureInPictureViewer } from './PictureInPictureViewer';
-export { default as SideBySideViewer } from './SideBySideViewer';
-export { default as VolumeViewer } from './VolumeViewer';
+// Keep extensions for documentation.js
+export { default as VivViewer } from './VivViewer.jsx';
+export { default as PictureInPictureViewer } from './PictureInPictureViewer.jsx';
+export { default as SideBySideViewer } from './SideBySideViewer.jsx';
+export { default as VolumeViewer } from './VolumeViewer.jsx';
