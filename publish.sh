c45a93f656c1f45849439fd8122a023fcf5a2c1c
Trevor Manz <trevor.j.manz@gmail.com>
2022-07-01 10:14:09 -0400

    feat: move publish script to github actions

--- publish.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-pnpm build --filter='./sites/*'
-aws s3 cp --recursive sites/avivator/dist s3://avivator.gehlenborglab.org
-aws s3 cp --recursive sites/docs/dist s3://viv.gehlenborglab.org
-echo "New demo site at avivator.gehlenborglab.org and docs at viv.gehlenborglab.org"
