985c415d2be4584aa52093d339fcad8eadb3741f
Trevor Manz <trevor.j.manz@gmail.com>
2022-07-01 13:41:09 -0400

    only pin versions in packages

--- .meta-updater/main.mjs
+++ .meta-updater/main.mjs
@@ -9,28 +9,32 @@ const LUMAGL_VERSION = '8.5.13';
 const MATHGL_VERSION = '^3.5.7';
 const REACT_VERSION = '^16.8.0 || ^17.0.0';
 
-/** @typedef {import('@pnpm/types').Dependencies} Dependencies */
+
 /** @typedef {import('@pnpm/types').PackageManifest} PackageManifest */
 
 /**
  * Mutates package metadata in-place to pin specific package versions.
  *
- * @param {import('@pnpm/types').Dependencies} deps
+ * @param {PackageManifest} manifest
  */
-function pinVersions(deps = {}) {
-  for (let name of Object.keys(deps)) {
-    if (name.startsWith('@deck.gl/') || name === 'deck.gl') {
-      deps[name] = DECK_VERSION;
+function pinVersions(manifest) {
+  for (let key of /** @type {const} */ (['dependencies', 'devDependencies', 'peerDependencies'])) {
+    let deps = manifest[key] ?? {};
+    for (let name of Object.keys(deps)) {
+      if (name.startsWith('@deck.gl/') || name === 'deck.gl') {
+        deps[name] = DECK_VERSION;
+      }
+      if (name.startsWith('@luma.gl/') || name === 'luma.gl') {
+        deps[name] = LUMAGL_VERSION;
+      }
+      if (name.startsWith('@math.gl/')) {
+        deps[name] = MATHGL_VERSION;
+      }
+      if (name === 'react' || name === 'react-dom') {
+        deps[name] = REACT_VERSION;
+      }
     }
-    if (name.startsWith('@luma.gl/') || name === 'luma.gl') {
-      deps[name] = LUMAGL_VERSION;
-    }
-    if (name.startsWith('@math.gl/')) {
-      deps[name] = MATHGL_VERSION;
-    }
-    if (name === 'react' || name === 'react-dom') {
-      deps[name] = REACT_VERSION;
-    }
+
   }
 }
 
@@ -44,9 +48,8 @@ export default (/** @type {string} */ workspaceDir) => {
       /** @type {string} */ _dir
     ) => {
       const version = manifest.name === '@hms-dbmi/viv' ? VIV_VERSION : meta.version;
-      pinVersions(manifest.dependencies);
-      pinVersions(manifest.devDependencies);
-      pinVersions(manifest.peerDependencies);
+      // Only pin deps in @vivjs/*. Avivator should manually update.
+      manifest.name?.includes('@vivjs') && pinVersions(manifest);
       return { ...manifest, version };
     }
   };
