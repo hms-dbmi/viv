2a24b8bff98c029431963b5d23409b0a76e43dee
Eric Moerth <ericmoerth@Erics-MacBook-Pro.local>
2023-07-10 16:12:02 -0400

    Perspective Rendering Kind of Working

--- sites/playground/src/main.ts
+++ sites/playground/src/main.ts
@@ -7,6 +7,9 @@ import {VolumeRenderShaderPerspective} from '../jsm/shaders/VolumeShaderPerspect
 import {Volume} from "../jsm/misc/Volume.js";
 import {Arcball} from './arcball';
 
+// import OrbitUnlimitedControls from '@janelia/three-orbit-unlimited-controls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 /* eslint-disable */
 import * as loaders from '@vivjs/loaders';
 
@@ -25,7 +28,7 @@ let url = new URL(
 );
 let { data: resolutions, metadata } = await loaders.loadOmeTiff(url.href);
 
-console.log({ resolutions, metadata });
+// console.log({ resolutions, metadata });
 
 for (let [i, resolution] of Object.entries(resolutions)) {
   let dims = Object.fromEntries(
@@ -62,8 +65,8 @@ let volumeOrigin = await getVolume({
 let { data, ...dimensions } = volumeOrigin;
 pre.textContent = `loaded volume.\n${JSON.stringify(dimensions, null, 2)}`;
 
-console.log(volumeOrigin);
-console.log(volumeOrigin.data.length)
+// console.log(volumeOrigin);
+// console.log(volumeOrigin.data.length)
 
 /**************************************************************
  * ************************************************************
@@ -84,14 +87,15 @@ volume.data = volumeOrigin.data;
 let container = Object.assign(document.createElement('div'), {});
 document.body.appendChild(container);
 container.style.height = "1000px";
-var canvas = document.createElement('canvas');
+container.style.background = "black";
+// var canvas = document.createElement('canvas');
 
 // get the min and max intensities
 var min_max = volume.computeMinMax();
 var min = min_max[0];
 var max = min_max[1];
 
-console.log(min + " " + max)
+// console.log(min + " " + max)
 
 var dataASFloat32 = new Float32Array(volume.data.length);
 for (var i = 0; i < volume.data.length; i++) {
@@ -100,28 +104,30 @@ for (var i = 0; i < volume.data.length; i++) {
 volume.data = dataASFloat32;
 min_max = volume.computeMinMax();
 
-console.log("after min max: " + min_max)
-console.log(volumeOrigin)
-console.log(volume)
+// console.log("after min max: " + min_max)
+// console.log(volumeOrigin)
+// console.log(volume)
 
 let scene = new THREE.Scene();
-var context = canvas.getContext('webgl2', {alpha: false, antialias: true});
-let renderer = new THREE.WebGLRenderer({canvas: canvas, context: context});
+// var context = canvas.getContext('webgl2', {alpha: false, antialias: true});
+// let renderer = new THREE.WebGLRenderer({canvas: canvas, context: context});
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
+// renderer.setClearColor("#000000");
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(container.offsetWidth, container.offsetHeight);
-while (container.childNodes.length > 0) {
-  container.removeChild(container.childNodes[0]);
-}
+// while (container.childNodes.length > 0) {
+//   container.removeChild(container.childNodes[0]);
+// }
+// renderer.setTarget(container)
 container.appendChild(renderer.domElement);
-
 
 var user = new THREE.Group();
 user.position.set(0,0,0);
-
 // Setting up the CAMERA for the THREE.js Rendering
 
-var h = 500; // frustum height
-var aspect = container.offsetWidth / container.offsetHeight;
+// var h = 500; // frustum height
+// var aspect = container.offsetWidth / container.offsetHeight;
 // let camera = new THREE.OrthographicCamera(-h * aspect / 2,
 //   h * aspect / 2,
 //   h / 2,
@@ -129,12 +135,13 @@ var aspect = container.offsetWidth / container.offsetHeight;
 //   1,
 //   100000);
 // camera.up.set(0, 0, 1); // In our data, z is up
-const camera = new THREE.PerspectiveCamera( 45, 1, 1, 10000 );
-camera.position.set(0, 0, -2);
+const camera = new THREE.PerspectiveCamera( 45, container.offsetWidth/container.offsetHeight, 0.01, 100000);
+camera.position.set(0, 0, 500);
+camera.up.set(0,0,1);
 user.add(camera);
 
 // camera.zoom = 1.8;
-let volconfig = {clim1: 0.15, clim2: 0.8, renderstyle: 'dvr' , isothreshold: 0.15, opacity: 1.0, colormap: 'viridis'};
+let volconfig = {clim1: 0.2, clim2: 0.8, renderstyle: 'dvr' , isothreshold: 0.15, opacity: 1.0, colormap: 'viridis'};
 
 // @ts-ignore
 var texture = new THREE.Data3DTexture(volume.data, volume.xLength, volume.yLength, volume.zLength);
@@ -156,19 +163,19 @@ var uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 // uniforms["u_data"].value = texture;
 uniforms["boxSize"].value.set(volume.xLength, volume.yLength, volume.zLength);
 uniforms["volumeTex"].value = texture;
-uniforms["near"].value = 1;
-uniforms["far"].value = 1000;
+uniforms["near"].value = 0.01;
+uniforms["far"].value = 100000;
 uniforms["alphaScale"].value = 1.0;
 uniforms["dtScale"].value = 1;
 uniforms["finalGamma"].value = 4.5;
 uniforms["useVolumeMirrorX"].value = false;
 // uniforms["u_size"].value.set(volume.xLength, volume.yLength, volume.zLength);
-// uniforms["u_clim"].value.set(volconfig.clim1, volconfig.clim2);
+uniforms["u_clim"].value.set(volconfig.clim1, volconfig.clim2);
 // uniforms["u_renderstyle"].value = volconfig.renderstyle === 'mip' ? 0 : volconfig.renderstyle === 'iso' ? 1 : 2; // 0: MIP, 1: ISO
 // uniforms["u_renderthreshold"].value = volconfig.isothreshold; // For ISO renderstyle
 // uniforms["u_opacity"].value = volconfig.opacity;
 // @ts-ignore
-// uniforms["u_cmdata"].value = cmtextures[volconfig.colormap];
+uniforms["u_cmdata"].value = cmtextures[volconfig.colormap];
 
 let material = new THREE.ShaderMaterial({
   uniforms: uniforms,
@@ -184,30 +191,50 @@ material.customProgramCacheKey = function () {
 };
 // THREE.Mesh
 var geometry = new THREE.BoxGeometry(volume.xLength, volume.yLength, volume.zLength);
-geometry.translate(volume.xLength / 2 - 0.5, volume.yLength / 2 - 0.5, volume.zLength / 2 - 0.5);
 var mesh = new THREE.Mesh(geometry, material);
 mesh.scale.set(1,1,4);
-mesh.position.set(-volume.xLength / 2, -volume.yLength / 2, -volume.zLength / 2); //if gi
 scene.add(mesh)
+
 
 scene.add(user);
 camera.updateProjectionMatrix();
 
-let arcball = new Arcball(renderer, camera, scene, container, mesh, user);
-container.addEventListener('mousedown', (event) => {
-  arcball.onDocumentMouseDown(event)
-}, false);
-container.addEventListener('mouseup', (event) => {
-  arcball.onDocumentMouseUp(event);
-}, false);
-container.addEventListener('mousemove', (event) => {
-  arcball.onDocumentMouseMove(event)
-}, false);
-container.addEventListener('wheel', (event) => {
-  arcball.onDocumentMouseWheel(event);
-}, false);
-arcball.animate()
+// const trackball = new OrbitUnlimitedControls(camera, renderer.domElement);
+// trackball.target.set(0, 0, -1);
+// trackball.zoomSpeed = 0.15;
+
+// Match the modifier keys used by VVD_Viewer, as described in the FluoRender user manual:
+// http://www.sci.utah.edu/releases/fluorender_v2.20/FluoRender2.20_Manual.pdf
+// Appendix "C. Keyboard Shortcuts"
+// trackball.usePanModAlt = true;
+// trackball.usePanModCtrl = true;
+// trackball.usePanModMeta = true;
 
 
+// let arcball = new Arcball(renderer, camera, scene, container, mesh, user);
+// container.addEventListener('mousedown', (event) => {
+//   arcball.onDocumentMouseDown(event)
+// }, false);
+// container.addEventListener('mouseup', (event) => {
+//   arcball.onDocumentMouseUp(event);
+// }, false);
+// container.addEventListener('mousemove', (event) => {
+//   arcball.onDocumentMouseMove(event)
+// }, false);
+// container.addEventListener('wheel', (event) => {
+//   arcball.onDocumentMouseWheel(event);
+// }, false);
+// arcball.animate()
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.update();
 document.body.appendChild(VRButton.createButton( renderer ) );
 renderer.xr.enabled = true;
+renderer.setAnimationLoop(() => animate());
+
+function animate() {
+  // requestAnimationFrame( animate );
+  // required if controls.enableDamping or controls.autoRotate are set to true
+  controls.update();
+  renderer.render( scene, camera );
+}
