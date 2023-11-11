46052984292e46856596d166c02e1c94c11855bf
Trevor Manz <trevor.j.manz@gmail.com>
2023-11-11 10:29:40 -0500

    handle tiffdata

--- packages/loaders/src/index.ts
+++ packages/loaders/src/index.ts
@@ -1,5 +1,6 @@
 export { loadOmeTiff, loadMultiTiff } from './tiff';
 export { loadBioformatsZarr, loadOmeZarr } from './zarr';
+export { load } from './tiff/multifile-ome';
 
 export { default as TiffPixelSource } from './tiff/pixel-source';
 export { default as ZarrPixelSource } from './zarr/pixel-source';
--- packages/loaders/src/tiff/multifile-ome.ts
+++ packages/loaders/src/tiff/multifile-ome.ts
@@ -1,96 +1,7 @@
-import { ensureArray } from "../utils";
-import { fromUrl, type GeoTIFF } from "geotiff";
-import { getOmePixelSourceMeta, type OmeTiffSelection } from "./lib/utils";
-import TiffPixelSource from "./pixel-source";
-
-function convertString(value: string): string | number | boolean {
-  // Attempt to convert to number
-  const numValue = parseFloat(value);
-  if (!isNaN(numValue)) {
-    return numValue;
-  }
-  // Attempt to convert to boolean
-  if (value.toLowerCase() === "true") {
-    return true;
-  } else if (value.toLowerCase() === "false") {
-    return false;
-  }
-  // Default to string
-  return value;
-}
-
-function isElement(node: Node): node is HTMLElement {
-  return node.nodeType === 1;
-}
-
-function isText(node: Node): node is Text {
-  return node.nodeType === 3;
-}
-
-type JsonValue =
-  | string
-  | number
-  | boolean
-  | { [x: string]: JsonValue }
-  | Array<JsonValue>;
-
-function xmlToJson(
-  xmlNode: HTMLElement,
-  options: { attrNodeName: string },
-): JsonValue | string | number | boolean {
-  if (isText(xmlNode)) {
-    // If the node is a text node
-    return convertString(xmlNode.nodeValue?.trim() ?? "");
-  }
-
-  // If the node has no attributes and no children, return an empty string
-  if (
-    xmlNode.childNodes.length === 0 &&
-    (!xmlNode.attributes || xmlNode.attributes.length === 0)
-  ) {
-    return "";
-  }
-
-  const jsonObj: JsonValue = {};
-
-  if (xmlNode.attributes && xmlNode.attributes.length > 0) {
-    const attrsObj: Record<string, string | boolean | number> = {};
-    for (let i = 0; i < xmlNode.attributes.length; i++) {
-      const attr = xmlNode.attributes[i];
-      attrsObj[attr.name] = convertString(attr.value);
-    }
-    jsonObj[options.attrNodeName] = attrsObj;
-  }
-
-  for (let i = 0; i < xmlNode.childNodes.length; i++) {
-    const childNode = xmlNode.childNodes[i];
-    if (!isElement(childNode)) {
-      continue;
-    }
-    const childJson = xmlToJson(childNode, options);
-    if (childJson !== undefined && childJson !== "") {
-      if (childNode.nodeName === "#text" && xmlNode.childNodes.length === 1) {
-        return childJson;
-      }
-      if (jsonObj[childNode.nodeName]) {
-        if (!Array.isArray(jsonObj[childNode.nodeName])) {
-          jsonObj[childNode.nodeName] = [jsonObj[childNode.nodeName]];
-        }
-        (jsonObj[childNode.nodeName] as JsonValue[]).push(childJson);
-      } else {
-        jsonObj[childNode.nodeName] = childJson;
-      }
-    }
-  }
-
-  return jsonObj;
-}
-
-export function parseXML(xmlStr: string) {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(xmlStr, "application/xml");
-  return xmlToJson(doc.documentElement, { attrNodeName: "attr" });
-}
+/* eslint-disable */
+import { fromUrl, type GeoTIFF } from 'geotiff';
+import type { OmeTiffSelection } from './lib/utils';
+import { fromString, type OMEXML } from '../omexml';
 
 function assert(condition: unknown, message?: string): asserts condition {
   if (!condition) {
@@ -98,41 +9,45 @@ function assert(condition: unknown, message?: string): asserts condition {
   }
 }
 
-function lookupKey({ c, t, z }: OmeTiffSelection): string {
+function keyFor({ c, t, z }: OmeTiffSelection): string {
   return `t${t}.c${c}.z${z}`;
 }
 
-export async function load(url: URL) {
-  assert(url.pathname.endsWith(".ome"), "Not a valid multifile ome extension");
-  const text = await fetch(url).then((res) => res.text());
-  const xml = parseXML(text);
+function createIndexer(
+  tiffData: NonNullable<OMEXML[0]['Pixels']['TiffData']>,
+  baseUrl: URL
+) {
+  const tiffs = new Map<string, Promise<GeoTIFF>>();
+  const lookup = new Map<string, { ifd: number; filename: string }>();
 
-  const tiffs = new Map<string, Promise<GeoTIFF>>;
-  const lookup = new Map<string, { ifd: number, filename: string }>();
-  for (const d of ensureArray(xml["Image"])[0]["Pixels"]["TiffData"]) {
-    const filename = d["UUID"].attr["FileName"];
-    const sel: OmeTiffSelection = { c: d.attr.FirstC, t: d.attr.FirstT, z: d.attr.FirstZ };
-    lookup.set(lookupKey(sel), { filename, ifd: d.attr["IFD"] });
+  for (const d of tiffData) {
+    const filename = d['UUID'].attr['FileName'];
+    const key = keyFor({
+      c: d.attr['FirstC'],
+      t: d.attr['FirstT'],
+      z: d.attr['FirstZ']
+    });
+    lookup.set(key, { filename, ifd: d.attr['IFD'] });
     if (!tiffs.has(filename)) {
-      tiffs.set(filename, fromUrl(new URL(filename, url).href));
+      tiffs.set(filename, fromUrl(new URL(filename, baseUrl).href));
     }
   }
 
-  const { attr, ...rest } = xml["Image"]["Pixels"];
-  const meta = getOmePixelSourceMeta({ Pixels: { ...rest, ...attr }});
-
-  async function indexer(selection: OmeTiffSelection) {
-    const entry = lookup.get(lookupKey(selection));
-    assert(entry, `No image for selection: ${JSON.stringify(selection)}`)
+  return async (selection: OmeTiffSelection) => {
+    const entry = lookup.get(keyFor(selection));
+    assert(entry, `No image for selection: ${JSON.stringify(selection)}`);
     const tiff = await tiffs.get(entry.filename)!;
     return tiff.getImage(entry.ifd);
-  }
-  const pixelSource = new TiffPixelSource(
-    indexer,
-    meta.dtype,
-    512,
-    meta.getShape(0),
-    meta.labels,
-  );
-  console.log(pixelSource)
+  };
+}
+
+export async function load(url: URL) {
+  assert(url.pathname.endsWith('.ome'), 'Not a valid multifile ome extension');
+  const text = await fetch(url).then(res => res.text());
+  console.log(text);
+  const omexml = fromString(text);
+  const tiffData = omexml[0]['Pixels']['TiffData'];
+  assert(tiffData, 'companion OME-XML is missing TiffData');
+  const indexer = createIndexer(tiffData, url);
+  return indexer;
 }
