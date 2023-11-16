cf02ac79af4e14ae8c915545f0ea06edd748bd44
Trevor Manz <trevor.j.manz@gmail.com>
2023-11-16 15:57:54 -0500

    chore: Remove serve static stuff

--- sites/avivator/vite.config.js
+++ sites/avivator/vite.config.js
@@ -1,39 +1,6 @@
 import { defineConfig } from 'vite';
-import serveStatic from 'serve-static';
 import react from '@vitejs/plugin-react';
 
-import * as path from 'path';
-
-/**
- * Vite plugins. Serves contents of `avivator/data` during development.
- * @param {string} dir
- * @returns {import('vite').Plugin}
- */
-function serveData(dir) {
-  if (dir[0] === '~') {
-    dir = path.join(process.env.HOME, dir.slice(1));
-  } else {
-    dir = path.resolve(__dirname, dir);
-  }
-  const serve = serveStatic(dir);
-  return {
-    name: 'serve-data-dir',
-    apply: 'serve',
-    configureServer(server) {
-      server.middlewares.use((req, res, next) => {
-        if (/^\/@data\//.test(req.url)) {
-          req.url = req.url.replace('/@data/', '');
-          serve(req, res, next);
-        } else {
-          next();
-        }
-      });
-    }
-  };
-}
-
-const dir = process.env.VIV_DATA_DIR || './data';
-
 export default defineConfig({
-  plugins: [react(), serveData(dir)]
+  plugins: [react()]
 });
