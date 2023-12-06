b0b68ec99de0f21adfc2f6460df52e94f7506f5e
Peter Todd <ptr.tdd@gmail.com>
2023-12-06 11:27:13 +0000

    some documentation

--- sites/avivator/src/state.js
+++ sites/avivator/src/state.js
@@ -155,8 +155,15 @@ const createViewerStore = () =>
       })
   }));
 
-export const AvivatorContext = createContext(null);
+const AvivatorContext = createContext(null);
 
+/**
+ * AvivatorProvider is used to provide `channels`, `imageSettings`, and `viewer` stores to the rest of the app.
+ * In the case of Avivator itself, there is one of these at the root, in `index.jsx`.
+ *
+ * You can have multiple independent viewers, using different images and/or other settings,
+ * by creating an AvivatorProvider for each viewer.
+ */
 export const AvivatorProvider = ({ children }) => {
   const storesRef = useRef(null);
   if (!storesRef.current) {
@@ -173,24 +180,45 @@ export const AvivatorProvider = ({ children }) => {
   );
 };
 
+/**
+ * useStoreApi is a basic wrapper function used to get a specific store from the AvivatorProvider.
+ *
+ * The `use<X>Store` functions are used to get state from the store, with a selector and optional equality function.
+ * These are used in Avivator components primarily to get state from the store and subscribe to changes.
+ * They cannot be used outside of the React render cycle, or anywhere where there is not an active {@link AvivatorContext}.
+ * In these cases, it is necessary to use the `use<X>StoreApi` functions instead.
+ *
+ * The `use<X>StoreApi` functions are used to obtain a reference to the store object itself, which can be later used
+ * outside of the React render cycle - for example, with `setState()` in an event handler - where the {@link AvivatorContext}
+ * would otherwise be ambiguous.
+ */
 function useStoreApi(storeName) {
   const store = useContext(AvivatorContext);
-  if (!store) throw 'useStore must be used within a AvivatorProvider';
+  if (!store) throw 'useStore must be used within an AvivatorProvider';
   return store[storeName];
 }
 export const useChannelsStoreApi = () => useStoreApi('channels');
 export const useImageSettingsStoreApi = () => useStoreApi('imageSettings');
 export const useViewerStoreApi = () => useStoreApi('viewer');
-function useAviStore(storeName, selector, eqFn) {
+/**
+ * For use of stores with `selector` and optional `equalityFn` within React rendering.
+ * Equivalent API to `use<X>Store` functions that were used before adding support for React Context,
+ * but only for invoking as a function, not `useXStore.setState(...)` etc.
+ *
+ * @param {'channels' | 'imageSettings' | 'viewer'} storeName
+ * @param {function} selector - selector function, see Zustand docs
+ * @param {function?} equalityFn - equality function, see Zustand docs
+ */
+function useAvivatorStore(storeName, selector, equalityFn) {
   const store = useStoreApi(storeName);
-  return useStoreWithEqualityFn(store, selector, eqFn);
+  return useStoreWithEqualityFn(store, selector, equalityFn);
 }
-export const useChannelsStore = (selector, eqFn) =>
-  useAviStore('channels', selector, eqFn);
-export const useImageSettingsStore = (selector, eqFn) =>
-  useAviStore('imageSettings', selector, eqFn);
-export const useViewerStore = (selector, eqFn) =>
-  useAviStore('viewer', selector, eqFn);
+export const useChannelsStore = (selector, equalityFn) =>
+  useAvivatorStore('channels', selector, equalityFn);
+export const useImageSettingsStore = (selector, equalityFn) =>
+  useAvivatorStore('imageSettings', selector, equalityFn);
+export const useViewerStore = (selector, equalityFn) =>
+  useAvivatorStore('viewer', selector, equalityFn);
 
 export const useLoader = () => {
   const [fullLoader, image] = useChannelsStore(store => [
