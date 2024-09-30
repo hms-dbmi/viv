7a1dd44b3ac31aadc7cc12dc0ab097f260147c7e
Peter Todd <ptr.tdd@gmail.com>
2024-09-30 12:44:34 +0100

    remove apparently redundant and deprecated gl alignment parameter setting.

--- packages/layers/src/xr-layer/xr-layer.js
+++ packages/layers/src/xr-layer/xr-layer.js
@@ -90,16 +90,6 @@ const XRLayer = class extends Layer {
    * This function initializes the internal state.
    */
   initializeState() {
-    const { device } = this.context;
-    // This tells WebGL how to read row data from the texture.  For example, the default here is 4 (i.e for RGBA, one byte per channel) so
-    // each row of data is expected to be a multiple of 4.  This setting (i.e 1) allows us to have non-multiple-of-4 row sizes.  For example, for 2 byte (16 bit data),
-    // we could use 2 as the value and it would still work, but 1 also works fine (and is more flexible for 8 bit - 1 byte - textures as well).
-    // https://stackoverflow.com/questions/42789896/webgl-error-arraybuffer-not-big-enough-for-request-in-case-of-gl-luminance
-    // -- this is now applying relevant parameters, but not helping with the rendering...
-    device.setParametersWebGL({
-      [GL.UNPACK_ALIGNMENT]: 1,
-      [GL.PACK_ALIGNMENT]: 1
-    });
     const attributeManager = this.getAttributeManager();
     attributeManager.add({
       positions: {
