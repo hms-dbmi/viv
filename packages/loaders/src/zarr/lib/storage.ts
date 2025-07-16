5bfd8049d3def11c3b6a6baf7cda39dea69889b0
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-07-16 12:31:42 -0400

    Use ZarrPixelSource from Vizarr

--- packages/loaders/src/zarr/lib/storage.ts
+++ packages/loaders/src/zarr/lib/storage.ts
@@ -1,5 +1,5 @@
-import { KeyError } from 'zarr';
-import type { AsyncStore } from 'zarr/types/storage/types';
+import { KeyError } from 'zarrita';
+import type { AsyncReadable } from 'zarrita';
 
 /**
  * Preserves (double) slashes earlier in the path, so this works better
@@ -16,30 +16,13 @@ function joinUrlParts(...args: string[]) {
     .join('/');
 }
 
-class ReadOnlyStore {
-  async keys() {
-    return [];
-  }
-
-  async deleteItem() {
-    return false;
-  }
-
-  async setItem() {
-    console.warn('Cannot write to read-only store.');
-    return false;
-  }
-}
-
 export class FileStore
-  extends ReadOnlyStore
-  implements AsyncStore<ArrayBuffer>
+  implements AsyncReadable
 {
   private _map: Map<string, File>;
   private _rootPrefix: string;
 
   constructor(fileMap: Map<string, File>, rootPrefix = '') {
-    super();
     this._map = fileMap;
     this._rootPrefix = rootPrefix;
   }
@@ -48,17 +31,13 @@ export class FileStore
     return joinUrlParts(this._rootPrefix, key);
   }
 
-  async getItem(key: string) {
+  async get(key: string) {
     const file = this._map.get(this._key(key));
     if (!file) {
       throw new KeyError(key);
     }
+    // Reference: https://github.com/manzt/zarrita.js/pull/161
     const buffer = await file.arrayBuffer();
-    return buffer;
-  }
-
-  async containsItem(key: string) {
-    const path = this._key(key);
-    return this._map.has(path);
+    return new Uint8Array(buffer);
   }
 }
