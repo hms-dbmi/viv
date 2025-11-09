973223aec72697b4e74dfb1292862579cba5f2a7
Peter Todd <ptr.tdd@gmail.com>
2025-11-09 08:02:59 +0000

    use imported template string in xr-fragment

--- packages/layers/src/xr-layer/xr-layer-fragment.glsl.js
+++ packages/layers/src/xr-layer/xr-layer-fragment.glsl.js
@@ -1,3 +1,4 @@
+import { VIV_CHANNEL_INDEX_PLACEHOLDER as I } from "../xr-layer/viv-shader-assembler";
 export default `\
 #version 300 es
 #define SHADER_NAME xr-layer-fragment-shader
@@ -6,7 +7,7 @@ precision highp int;
 precision highp SAMPLER_TYPE;
 
 // our texture
-uniform SAMPLER_TYPE channel<VIV_CHANNEL_INDEX>;
+uniform SAMPLER_TYPE channel${I};
 
 in vec2 vTexCoord;
 
@@ -15,22 +16,21 @@ in vec2 vTexCoord;
 uniform vec2 contrastLimits[NUM_CHANNELS];
 // the token after uniform is one that will be used in JS (with or without Uniforms)
 // uniform xrLayerUniforms {
-//   // this is going to be a nuisance to associate with props...
-//   vec2 contrastLimits<VIV_CHANNEL_INDEX>;
+//   vec2 contrastLimits${I};
 // } xrLayer;
 
 out vec4 fragColor;
 
 void main() {
 
-  float intensity<VIV_CHANNEL_INDEX> = float(texture(channel<VIV_CHANNEL_INDEX>, vTexCoord).r);
-  DECKGL_PROCESS_INTENSITY(intensity<VIV_CHANNEL_INDEX>, contrastLimits[<VIV_CHANNEL_INDEX>], <VIV_CHANNEL_INDEX>);
-  // DECKGL_PROCESS_INTENSITY(intensity<VIV_CHANNEL_INDEX>, xrLayer.contrastLimits<VIV_CHANNEL_INDEX>, <VIV_CHANNEL_INDEX>);
+  float intensity${I} = float(texture(channel${I}, vTexCoord).r);
+  DECKGL_PROCESS_INTENSITY(intensity${I}, contrastLimits[${I}], ${I});
+  // DECKGL_PROCESS_INTENSITY(intensity${I}, xrLayer.contrastLimits${I}, ${I});
 
   float[] intensity = float[NUM_CHANNELS](
     // as of this writing, this will be expanded by some very fragile string processing to remove final comma...
     // needs documenting and hopefully improving.
-    intensity<VIV_CHANNEL_INDEX>,
+    intensity${I},
   );
   DECKGL_MUTATE_COLOR(fragColor, intensity, vTexCoord);
 
