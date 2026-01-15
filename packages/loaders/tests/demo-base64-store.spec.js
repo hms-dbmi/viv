241289e2ba5bceaf235387d2504fa109501c3a69
Peter Todd <ptr.tdd@gmail.com>
2026-01-15 17:05:47 +0000

    use ome-zarr test-fixtures from spatialdata.datasets.blobs, checked in to repo.

--- packages/loaders/tests/demo-base64-store.spec.js
+++ packages/loaders/tests/demo-base64-store.spec.js
@@ -2,8 +2,8 @@ import { expect, test } from 'vitest';
 import { createStoreFromMapContents } from './base64-store';
 import * as zarr from 'zarrita';
 
-import ome_ngff_0_4_fixture from './fixtures/ome-zarr/v0_4_idr0076A_10501752.ome.zarr.json';
-import ome_ngff_0_5_fixture from './fixtures/ome-zarr/v0_5_idr0062A_6001240_labels.ome.zarr.json';
+import ome_ngff_0_4_fixture from './fixtures/ome-zarr/blobs_image_v04.ome.zarr.json';
+import ome_ngff_0_5_fixture from './fixtures/ome-zarr/blobs_image_v05.ome.zarr.json';
 
 test('Check that JSON-based store fixtures can be loaded: NGFF v0.4', async () => {
   const store = createStoreFromMapContents(ome_ngff_0_4_fixture);
@@ -14,7 +14,8 @@ test('Check that JSON-based store fixtures can be loaded: NGFF v0.4', async () =
 
   expect(group.attrs).toBeTruthy();
 
-  expect(Object.keys(group.attrs)).toEqual(['_creator', 'multiscales', 'omero']);
+  // Check for required OME-NGFF v0.4 metadata (may have additional SpatialData attrs)
+  expect(Object.keys(group.attrs)).toEqual(expect.arrayContaining(['multiscales', 'omero']));
 });
 
 test('Check that JSON-based store fixtures can be loaded: NGFF v0.5', async () => {
@@ -26,7 +27,8 @@ test('Check that JSON-based store fixtures can be loaded: NGFF v0.5', async () =
 
   expect(group.attrs).toBeTruthy();
 
-  expect(Object.keys(group.attrs)).toEqual(['ome']);
-  expect(Object.keys(group.attrs['ome'])).toEqual(['version', '_creator', 'multiscales', 'omero']);
+  // Check for required OME-NGFF v0.5 metadata (may have additional SpatialData attrs)
+  expect(Object.keys(group.attrs)).toEqual(expect.arrayContaining(['ome']));
+  expect(Object.keys(group.attrs['ome'])).toEqual(expect.arrayContaining(['omero', 'version', 'multiscales']));
 });
 
