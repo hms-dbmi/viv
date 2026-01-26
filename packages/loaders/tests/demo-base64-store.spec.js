9949de6e8159073f67003f52931fedc788b360af
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2026-01-26 14:51:06 -0500

    Test loadOme function

--- packages/loaders/tests/demo-base64-store.spec.js
+++ packages/loaders/tests/demo-base64-store.spec.js
@@ -1,6 +1,8 @@
 import { expect, test } from 'vitest';
 import * as zarr from 'zarrita';
 import { createStoreFromMapContents } from './base64-store';
+import { load as loadOme } from '../src/zarr/ome-zarr';
+
 
 import ome_ngff_0_4_fixture from './fixtures/ome-zarr/blobs_image_v04.ome.zarr.json';
 import ome_ngff_0_5_fixture from './fixtures/ome-zarr/blobs_image_v05.ome.zarr.json';
@@ -34,4 +36,27 @@ test('Check that JSON-based store fixtures can be loaded: NGFF v0.5', async () =
   expect(Object.keys(group.attrs['ome'])).toEqual(
     expect.arrayContaining(['omero', 'version', 'multiscales'])
   );
+});
+
+
+test('Test loadOme function with NGFF v0.4 image', async () => {
+  const store = createStoreFromMapContents(ome_ngff_0_4_fixture);
+  const { data, metadata } = await loadOme(store);
+
+  expect(data).toBeTruthy();
+  expect(metadata).toBeTruthy();
+  expect(metadata.multiscales[0].name).toEqual('/images/blobs_image');
+  expect(data.length).toBe(1); // One resolution
+  expect(data[0].labels).toEqual(['c', 'y', 'x']);
+});
+
+test('Test loadOme function with NGFF v0.5 image', async () => {
+  const store = createStoreFromMapContents(ome_ngff_0_5_fixture);
+  const { data, metadata } = await loadOme(store);
+
+  expect(data).toBeTruthy();
+  expect(metadata).toBeTruthy();
+  expect(metadata.multiscales[0].name).toEqual('/images/blobs_image');
+  expect(data.length).toBe(1); // One resolution
+  expect(data[0].labels).toEqual(['c', 'y', 'x']);
 });
