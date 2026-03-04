621b7f253d3717cbb826ee4a19084f84d05fd793
ilan-gold <ilanbassgold@gmail.com>
2026-03-04 16:26:08 +0100

    fix: docs

--- packages/layers/src/scale-bar-layer.js
+++ packages/layers/src/scale-bar-layer.js
@@ -22,7 +22,7 @@ const defaultProps = {
  * @type {Object}
  * @property {String} unit Physical unit size per pixel at full resolution.
  * @property {Number} size Physical size of a pixel.
- * @property {Object} viewState The current viewState for the desired view.  We cannot internally use this.context.viewport because it is one frame behind:
+ * @property {Object} imageViewState The current viewState for the desired image.  We cannot internally use this.context.viewport because it is one frame behind:
  * https://github.com/visgl/deck.gl/issues/4504
  * @property {Array=} boundingBox Boudning box of the view in which this should render.
  * @property {string=} id Id from the parent layer.
